ffd1fde221bf5c733d0be0d4eea16e33b8b40cc8
Allison Bohannon <akboh.github@gmail.com>
2022-04-18 21:17:43 -0700

    commit tree 9b82e54ac19566a2067f477765d59a8a4cb9009a

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+return tutorials.map( title => {
+  const words = title.split(' '); 
+  return words.map( word => {
+    return word.charAt(0).toUpperCase() + word.slice(1); 
+  } ).join(' ')
+  
+}); 
 }
+
+console.log(titleCased()); 
+
